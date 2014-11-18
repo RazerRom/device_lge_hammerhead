236d49f975337c9996ff22e2c635ca422a1f18a7
Chet Kener <Cl3Kener@gmail.com>
2014-11-18 01:30:01 -0500

    Enable Workaround for slow ROM flash
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,3 +173,7 @@ USE_MINIKIN := true
 
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
+
+# Enable workaround for slow rom flash
+BOARD_SUPPRESS_SECURE_ERASE := true
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
