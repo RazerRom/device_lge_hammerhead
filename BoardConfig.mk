f2673abc84d7f5b10a4b036ba1c4588a1867329e
gamerman123x <19superguy98@gmail.com>
2015-02-19 21:40:20 -0500

    Build flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_NO_BOOTLOADER := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
@@ -111,13 +111,15 @@ BOARD_SEPOLICY_DIRS += \
 # Define kernel config for inline building
 TARGET_KERNEL_CONFIG := cyanogenmod_hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
-TARGET_GCC_VERSION_ARM := 4.9-sm
 
 #GamerMod Optimizations
+TARGET_TC_ROM := 4.8
+TARGET_TC_KERNEL := 4.9
 GAMERMOD := true
-GRAPHITE_OPTS := true
-STRICT_ALIASING := true
-
+GM_O3 := true
+GM_STRICT := true
+GM_GRAPHITE := true
+GM_BUILD_BLOCK := 0
 TOUCH_BOOST_DEBUG := false
 
 # The list below is order dependent
