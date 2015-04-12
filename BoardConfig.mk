a486d6f5a8e6d535fcff98d72f4f6371ef6c23ac
gamerman123x <19superguy98@gmail.com>
2015-04-12 17:10:09 -0400

    GamerMod Kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,7 @@ BOARD_SEPOLICY_DIRS += \
        device/lge/hammerhead/sepolicy
 
 # Define kernel config for inline building
-TARGET_KERNEL_CONFIG := cyanogenmod_hammerhead_defconfig
+TARGET_KERNEL_CONFIG := aosp_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 
 TOUCH_BOOST_DEBUG := false
